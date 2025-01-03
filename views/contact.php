727d2d159a6847240483aac71853ef73affbe743
yassine snaiki <yassnaiki@gmail.com>
2025-01-03 12:28:58 +0100

    .

---
+++ views/contact.php
@@ -0,0 +1,7 @@
+<h1>Contact</h1>
+<h2><?=$name?></h2>
+<form action="" method="POST">
+    <input type="text" name="name" id="name">
+    <input type="text" name="description" id="description">
+    <button type="submit">submit</button>
+</form>
