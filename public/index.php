7e280c6381c4fb88bf22369557777c62833ac757
yassine snaiki <yassnaiki@gmail.com>
2025-01-10 12:20:59 +0100

    adding routes for roles and permissions

--- public/index.php
+++ public/index.php
@@ -1,12 +1,19 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use app\controllers\SiteController;
+
 use app\controllers\AuthController;
+use app\controllers\ContributionController;
+use app\controllers\PermissionController;
 use app\controllers\ProjectController;
+use app\controllers\RoleController;
+use app\controllers\TaskController;
+use app\controllers\AdminController;
 use app\core\Application;
 
+
 session_start();
+
 
 $app = new Application(dirname(__DIR__));
 
@@ -21,15 +28,18 @@ $app->router->post('/register', [AuthController::class, 'handleRegister']);
 
 $app->router->get('/logout', [AuthController::class, 'logout']);
 
+// Admin routes
+$app->router->get('/admin-dashboard', [AdminController::class, 'adminDashboard']);
+
 // Project routes
 $app->router->get('/', [ProjectController::class, 'projects']);
-$app->router->post('/', [ProjectController::class, 'handleProjects']);
-// $app->router->post('/projects', [ProjectController::class, 'handleProjects']);
+$app->router->post('/add-project', [ProjectController::class, 'addProject']);
 
-// $app->router->get('/projects', [ProjectController::class, 'projects']);
+
+
 $app->router->post('/delete-project', [ProjectController::class, 'deleteProject']);
-$app->router->post('/add-contribution', [ProjectController::class, 'addContribution']);
-$app->router->post('/delete-contribution', [ProjectController::class, 'deleteContribution']);
+$app->router->post('/add-contribution', [ContributionController::class, 'addContribution']);
+$app->router->post('/delete-contribution', [ContributionController::class, 'deleteContribution']);
 
 
 
@@ -37,17 +47,29 @@ $app->router->post('/delete-contribution', [ProjectController::class, 'deleteCon
 $app->router->get('/kanban', [ProjectController::class, 'kanban']);
 
 
-$app->router->post('/add-task', [ProjectController::class, 'addTask']);
-$app->router->post('/update-task-status', [ProjectController::class, 'updateTaskStatus']);
-$app->router->post('/change-task-tag', [ProjectController::class, 'changeTaskTag']);
-$app->router->post('/delete-task', [ProjectController::class, 'deleteTask']);
-$app->router->post('/assign-task', [ProjectController::class, 'assignTask']);
-$app->router->post('/unassign-task', [ProjectController::class, 'unassignTask']);
+$app->router->post('/add-task', [TaskController::class, 'addTask']);
+$app->router->post('/update-task-status', [TaskController::class, 'updateTaskStatus']);
+$app->router->post('/change-task-tag', [TaskController::class, 'changeTaskTag']);
+$app->router->post('/delete-task', [TaskController::class, 'deleteTask']);
+$app->router->post('/assign-task', [TaskController::class, 'assignTask']);
+$app->router->post('/unassign-task', [TaskController::class, 'unassignTask']);
+
+
+$app->router->post('/change-role', [RoleController::class, 'changeRole']);
 
 
 
-// Other routes
-$app->router->get('/contact', [SiteController::class, 'contact']);
-$app->router->post('/contact', [SiteController::class, 'handleContact']);
+$app->router->post('/create-permission', [PermissionController::class, 'createPermission']);
+$app->router->post('/delete-permission', [PermissionController::class, 'deletePermission']);
+$app->router->post('/add-role', [RoleController::class, 'addRole']);
+$app->router->post('/delete-role', [RoleController::class, 'deleteRole']);
+$app->router->post('/add-permission', [RoleController::class, 'addPermission']);
+$app->router->post('/remove-permission', [RoleController::class, 'removePermission']);
+
+
+
+
+
+
 
 $app->run();
